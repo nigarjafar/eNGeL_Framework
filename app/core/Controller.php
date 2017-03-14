187b2dd60bfb6b69b0d4eb81d17808e9e3c543b3
Lala.M <lalamamedova97@gmail.com>
2017-03-14 14:02:54 +0400

    solve conflict

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,51 +1,32 @@
 <?php
 
-class Controller{
+class Controller
+{
 
-	public function model($model){
+	public function model($model)
+	{
 		require_once '../app/models/'.$model.'.php';
 		return new $model;
 	}
 
-	public function View($url, $data=[]){
-		if ($data!=null) {
+	public function View($url, $data=[])
+	{
+		if ($data!=null)
+		{
 			extract($data);
 		}
-		require_once '../app/views/'.$url.'.php';
+	 	require_once '../app/views/'.$url.'.php';
 	}
 
 
 
-	public function loader(){
-	  if (file_exists('../app/core/Loader.php')){
-	    require '../app/core/Loader.php';
-	      return new Loader();
-	    }
-<<<<<<< HEAD
-	}
-=======
+	public function loader()
+		{
+		  if (file_exists('../app/core/Loader.php'))
+			{
+		    require '../app/core/Loader.php';
+		    return new Loader();
+		  }
+		}
 
-  //   public function loadLib($name,$const=[]){
-	//     if (file_exists('../app/library/'.$name.'.php')){
-	//         require '../app/library/'. $name.'.php';
-	//
-	//         return new $name($const);
-  //       }
-  //       else{
-	//         echo $name.' movcud deyil. ';
-  //       }
-  //   }
-	// // public function DB($table){
-	// // 	require_once '../app/database/db.php';
-	// // 	return new db($table,new DBConnection());
-	// // }
-	//
-	// public function upload($config=[])
-	// {
-	// 		// $file=new fileConfig($config);
-	// 		  //  return $file->file_upload();
-	// 		return self::loadLib("fileConfig",$config)->file_upload();
-	// }
-
->>>>>>> ab3da853ac0ef0387bf279a8bc60c48544061451
 }
